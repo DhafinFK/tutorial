7c01665b33685d91eb21e14d156682686e688225
DhafinFK <dhafin.kamal@gmail.com>
2024-02-13 14:59:27 +0700

    removed unnecessary public modifiers

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,8 +4,8 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product edit(Product product);
-    public Product delete(Product product);
+    Product create(Product product);
+    List<Product> findAll();
+    Product edit(Product product);
+    Product delete(Product product);
 }
