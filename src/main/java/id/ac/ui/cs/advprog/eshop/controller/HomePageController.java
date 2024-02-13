38503fdb6a5770b9b7260d6c5d24f516c6e8c0e5
DhafinFK <dhafin.kamal@gmail.com>
2024-02-13 14:53:38 +0700

    removed unused imports

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -1,10 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 @Controller
 @RequestMapping("/")
 public class HomePageController {
